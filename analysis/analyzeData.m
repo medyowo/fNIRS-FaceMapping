7d46b621fe8a2b40c74e4b2a0086aa87bf85918e
TrLSeysh <trlseysh@gmail.com>
2024-06-13 10:52:29 +0900

    Modify display and get data function

--- analysis/analyzeData.m
+++ analysis/analyzeData.m
@@ -2,7 +2,7 @@ expArrays = getExpData();
 
 noise_yo = expArrays{:, 5};
 
-displayData(expArrays, 1, 6, ["oxyHb", "deoxyHb"])
-displayData(expArrays, 7, 12, ["oxyHb", "deoxyHb"])
-displayData(expArrays, 13, 18, ["oxyHb", "deoxyHb"])
-displayData(expArrays, 19, 22, ["oxyHb", "deoxyHb", "totalHb"])
+displayData(expArrays, 1, 2, 1, 6, ["oxyHb", "deoxyHb"])
+displayData(expArrays, 1, 1, 7, 12, ["oxyHb", "deoxyHb"])
+displayData(expArrays, 1, 1, 13, 18, ["oxyHb", "deoxyHb"])
+displayData(expArrays, 1, 1, 19, 22, ["oxyHb", "deoxyHb", "totalHb"])
--- analysis/displayData.m
+++ analysis/displayData.m
@@ -3,36 +3,37 @@
 % startValue : first channel to display
 % endValue : last channel to display
 % toDisplay : What value to display between [oxyHb, deoxyHb, totHb]
-function displayData(data, startValue, endValue, toDisplay)
-
-% Create figure for subplot channels
-figure('Name',['Visualisation of channels ', num2str(startValue), ' - ', num2str(endValue)], 'Numbertitle','off');
-
-% For each channel display values
-for k=0+startValue : endValue
-    % Display data in 2 columns
-    subplot(round((endValue-startValue)/2, TieBreaker="plusinf"),2,k-(startValue-1));
-
-    xlabel('time (seconds)');
-    ylabel('Hb');
-    title(['channel n°', num2str(k)])
-    hold on
-    
-    % Check each data which is needed to be displayed
-    if ismember("oxyHb", toDisplay)
-        plot(data{1}{:,2}, data{1}{:,6+3*(k-1)}, "Color", "#A2142F");
+function displayData(data, startData, EndData, startChannel, endChannel, toDisplay)
+    %for each data
+    for j=0+startData : EndData
+        % Create figure for subplot channels
+        figure('Name',['DATA N°', num2str(j),' - Visualisation of channels ', num2str(startChannel), ' - ', num2str(endChannel)], 'Numbertitle','off');
+        
+        % For each channel display values
+        for k=0+startChannel : endChannel
+            % Display data in 2 columns
+            subplot(round((endChannel-startChannel)/2, TieBreaker="plusinf"),2,k-(startChannel-1));
+        
+            xlabel('time (seconds)');
+            ylabel('Hb');
+            title(['channel n°', num2str(k)])
+            hold on
+            
+            % Check each data which is needed to be displayed
+            if ismember("oxyHb", toDisplay)
+                plot(data{j}{:,2}, data{j}{:,6+3*(k-1)}, "Color", "#A2142F");
+            end
+        
+            if ismember("deoxyHb", toDisplay)
+                plot(data{j}{:,2}, data{j}{:,7+3*(k-1)}, "Color", "#0072BD")
+            end
+        
+            if ismember("totalHb", toDisplay)
+                plot(data{j}{:,2}, data{j}{:,8+3*(k-1)}, "Color", "#77AC30")
+            end
+        
+            legend(toDisplay)
+            ylim([-1 1])
+            hold off
+        end
     end
-
-    if ismember("deoxyHb", toDisplay)
-        plot(data{1}{:,2}, data{1}{:,7+3*(k-1)}, "Color", "#0072BD")
-    end
-
-    if ismember("totalHb", toDisplay)
-        plot(data{1}{:,2}, data{1}{:,8+3*(k-1)}, "Color", "#77AC30")
-    end
-
-    legend(toDisplay)
-    ylim([-1 1])
-    hold off
-end
-
